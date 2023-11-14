6363373a6afb043d2e3162b75cca2a6b54e41360
Diego Aguilera <diegoaguilerareyes4@gmaIl.com>
2023-11-14 20:09:28 -0300

    se agrega pacientes a modelo profesional

--- src/models/profesional.js
+++ src/models/profesional.js
@@ -5,6 +5,7 @@ const profesionalSchema = new Schema({
   nombre: { type: String, required: true },
   email: { type: String, required: true },
   contraseña: { type: String, required: true },
+  pacientes: [{ type: Schema.Types.ObjectId, ref: "Paciente"}],
 });
 
 //convertir a modelo de mongoose
