f1c71df73726964177b1c3fa9bc42012cdab8aa7
diegoaguilera4 <diegoaguilerareyes4@gmail.com>
2023-11-27 19:29:15 -0300

    código de vinculación único

--- src/controllers/paciente.js
+++ src/controllers/paciente.js
@@ -1,15 +1,35 @@
 import Paciente from "../models/paciente.js";
 import miDB from "../db/index.js";
+import crypto from 'crypto';
 
-///agregar paciente
+
 export const agregarPaciente = async (req, res) => {
     try {
-        const paciente = new Paciente(req.body);
+        // Generar un código de vinculación único
+        const codigoVinculacion = generarCodigoVinculacion();
+
+        // Crear el objeto paciente con el código de vinculación generado
+        const paciente = new Paciente({
+            ...req.body,
+            codigoVinculacion,
+        });
+
+        // Guardar el paciente en la base de datos
         await paciente.save();
+
         res.status(201).json(paciente);
     } catch (error) {
         res.status(500).json({ mensaje: error.message });
     }
+};
+
+// Función para generar un código de vinculación único
+function generarCodigoVinculacion() {
+    const longitud = 4; // Puedes ajustar la longitud según tus necesidades
+    const bytes = crypto.randomBytes(longitud);
+    const codigo = bytes.toString('hex').toUpperCase(); // Convierte a cadena hexadecimal y mayúsculas
+
+    return codigo;
 }
 
 //obtener todos los pacientes
--- src/models/paciente.js
+++ src/models/paciente.js
@@ -4,9 +4,8 @@ import { Schema } from "mongoose";
 const pacienteSchema = new Schema({
     nombre: { type: String, required: true },
     rut: { type: String, required: true },
-    email: { type: String, required: true },
     ficha: { type: Schema.Types.ObjectId, ref: "Ficha"},
-    codigoVinculacion: { type: String, required: true },
+    codigoVinculacion: { type: String, required: true, unique: true},
     tareas: [{ type: Schema.Types.ObjectId, ref: "Tarea"}],
 });
 
